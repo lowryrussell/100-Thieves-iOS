e90ae38364f8aa780fb8102900c9b37af34ee614
Russell Lowry <lowryrussell@gmail.com>
2020-05-06 07:52:03 -0500

    Initial Commit

---
+++ HundredThieves/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  HundredThieves
+//
+//  Created by Russell Lowry on 5/6/20.
+//  Copyright © 2020 100Thieves. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HundredThieves/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  HundredThieves
+//
+//  Created by Russell Lowry on 5/6/20.
+//  Copyright © 2020 100Thieves. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
